82983fd843affe94f8ec473da0be6639052de980
Michal Darda <michald@telnyx.com>
2020-04-13 17:38:00 +0200

    Add tracing_fns

---
+++ chapter-5/sysstatistics_repl.erl
@@ -0,0 +1,8 @@
+c("chapter-4/frequency.erl").
+frequency:start().
+sys:statistics(frequency, true).
+frequency:allocate().
+frequency:allocate().
+frequency:allocate().
+frequency:allocate().
+sys:statistics(frequency,get).
